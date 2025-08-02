acc50da1624c2cdaa6660052cf361055379187d0
chrisdev-ts <christian.serrano.puertos@gmail.com>
2025-08-01 23:54:40 -0600

    feat: configure stack navigator with splash as initial route

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,13 @@
-import "../global.css";
 import { Stack } from "expo-router";
+import "../global.css";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack initialRouteName="splash">
+      <Stack.Screen name="splash" options={{ headerShown: false }} />
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="welcome" options={{ headerShown: false }} />
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+    </Stack>
+  );
 }
