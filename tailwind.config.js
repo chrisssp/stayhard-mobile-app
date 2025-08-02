09f10fe4c8c80ef6e941138baa0e64c2492615e5
chrisdev-ts <christian.serrano.puertos@gmail.com>
2025-08-01 23:56:29 -0600

    feat: configure Inter font as default typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  content: [
-    "./app/**/*.{js,jsx,ts,tsx}",
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Inter", "ui-sans-serif", "system-ui"],
+      },
+    },
   },
   plugins: [],
 };
